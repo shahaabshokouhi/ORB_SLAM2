267ab069fb352fca246e4862ae77c08da95b6402
Shahab <shokohishahab@gmail.com>
2026-06-11 18:52:06 -0400

    realsense.yaml as env var

--- live_rgbd.sh
+++ live_rgbd.sh
@@ -1,1 +1,6 @@
-./Examples/rgbd_live Vocabulary/ORBvoc.txt Examples/RGB-D/realsense.yaml
+if [ -z "$REALSENSE_CONFIG" ]; then
+    echo "Error: REALSENSE_CONFIG environment variable is not set."
+    echo "Set it in ~/.bashrc, e.g.: export REALSENSE_CONFIG=/path/to/realsense.yaml"
+    exit 1
+fi
+./Examples/rgbd_live Vocabulary/ORBvoc.txt "$REALSENSE_CONFIG"
